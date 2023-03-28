f2ce23e016068189a102307cb40022f15cd0bf5a
dariog98 <dariogoirad98@gmail.com>
2023-03-28 16:34:35 -0300

    Change data

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    base: 'https://github.com/dariog98/games-react',
+    //base: '/games-react/',
     plugins: [react()]
 })
